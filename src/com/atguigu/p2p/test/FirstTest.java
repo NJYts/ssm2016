f25b73ef391f8040ab622c3ff05698ce9e1c5db3
niujianye <niujianye@126.com>
2018-07-10 16:11:46 +0800

    新增：测试

--- src/com/atguigu/p2p/test/FirstTest.java
+++ src/com/atguigu/p2p/test/FirstTest.java
@@ -1,7 +1,7 @@
 package com.atguigu.p2p.test;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
+import java.io.*;
 
 public class FirstTest {
 	private static HttpServletRequest request;
@@ -13,6 +13,63 @@ public class FirstTest {
 		File file = new File(""+this.getClass().getClassLoader().getResource(""));
 		System.out.println(file.getAbsolutePath());
 	}
+
+
+	/**
+	 * 写入二进制
+	 * @param file
+	 */
+	public static void writeFile(File file){
+		if(file != null){
+			try {
+				FileOutputStream fos = new FileOutputStream(file);
+				DataOutputStream dos = new DataOutputStream(fos);
+				dos.writeUTF("abcdefg");
+				dos.writeUTF("学习");
+				dos.writeUTF("are you ok?");
+				dos.writeBoolean(true);
+				dos.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	/**
+	 * 读取二进制文件
+	 * @param file
+	 */
+	public static void readFile(File file){
+		if(file != null){
+			try {
+				FileInputStream fis = new FileInputStream(file);
+				DataInputStream dis = new DataInputStream(fis);
+				System.out.println(dis.readUTF());
+				System.out.println(dis.readUTF());
+				System.out.println(dis.readUTF());
+				System.out.println(dis.readBoolean());
+				/*while(dis.available() != 0){
+					System.out.print((char)dis.readByte());
+				}*/
+				dis.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+
 	public static void main(String[] args) {
 		String s= "墨西哥','巴拿马','尼加拉瓜','海地','多米尼加','古巴','哥斯达黎加','危地马拉','萨尔瓦多','洪都拉斯','玻利维亚','委内瑞拉','秘鲁','厄瓜多尔','哥伦比亚','巴西','阿根廷','巴拉圭','智利','乌拉圭','巴哈马群岛','格林纳达','牙买加','伯利兹','特立尼达和多巴哥','圣文森特','苏里南','圣基茨和尼维斯','多米尼克','圣卢西亚','圭亚那','安提瓜和巴布达";
 		String[] strs = s.split(",");
@@ -20,6 +77,10 @@ public class FirstTest {
 		System.out.println(strs.length);
 		/*FirstTest firstTest = new FirstTest();
 		firstTest.getUrl();*/
-		
+
+		File file  = new File("fileTest2.txt");
+		writeFile(file);
+		readFile(file);
+
 	}
 }
