c70c8c963a66bfd169a646e20e5d228b9e03ff60
niujianye <niujianye@126.com>
2018-11-28 10:32:12 +0800

    新增：算法文档

--- test/exerciseGoWork/shufa/ShellSort.java
+++ test/exerciseGoWork/shufa/ShellSort.java
@@ -15,11 +15,53 @@ import java.util.Arrays;
 public class ShellSort {
     public static void main(String[] args) {
         int[] arr = {1, 4, 2, 7, 9, 8, 3, 6};
-        sort(arr);
+        sort2(arr);
         System.out.println(Arrays.toString(arr));
         int[] arr1 = {1, 4, 2, 7, 9, 8, 3, 6};
-        sort1(arr1);
+        sort3(arr1);
         System.out.println(Arrays.toString(arr1));
+    }
+
+    /**
+     * 希尔排序 针对有序序列在插入时采用交换法
+     *
+     * @param arr
+     */
+    public static void sort2(int[] arr) {
+        //增量gap，并逐步缩小增量
+        for (int gap = arr.length / 2;  gap > 0; gap/=2) {
+            //从第gap个元素，逐个对其所在组进行直接插入排序操作
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                while (j - gap >= 0 && arr[j] < arr[j - gap]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - gap];
+                    arr[j - gap] = temp;
+                    j -= gap;
+                }
+            }
+        }
+
+                    //插入排序采用交换法
+    }
+
+    /**
+     * 希尔排序 针对有序序列在插入时采用移动法。
+     *
+     * @param arr
+     */
+    public static void sort3(int[] arr) {
+        //控制增量gap，并逐步缩小增量
+        for (int gap = arr.length/2; gap > 0; gap/=2) {
+            for (int i = gap; i < arr.length; i++) {
+                int temp=arr[i];
+                while (i - gap >= 0 && temp < arr[i - gap]) {
+                    arr[i] = arr[i - gap];
+                    i-=gap;
+                }
+                arr[i] = temp;
+            }
+        }
     }
 
     /**
